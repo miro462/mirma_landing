6d23a62be25baf76ddbf0f96fc12ba23249dbf0d
miro462 <marcmt40@gmail.com>
2025-07-02 22:17:20 +0200

    añadir favicon

--- js/cookies.js
+++ js/cookies.js
@@ -1,27 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const cookieBanner = document.getElementById('cookie-banner');
   const acceptBtn = document.getElementById('accept-cookies');
-  const rejectBtn = document.getElementById('reject-cookies');
 
-  // Mostrar el banner solo si no hay decisión guardada
-  const decision = localStorage.getItem('cookiesDecision');
-  if (decision === 'accepted' || decision === 'rejected') {
-    cookieBanner.classList.add('hidden');
-  } else {
+  // Mostrar el banner solo si no se ha aceptado aún
+  if (!localStorage.getItem('cookiesAccepted')) {
     cookieBanner.classList.remove('hidden');
   }
 
-  // Función para ocultar banner y guardar decisión
-  function handleDecision(decision) {
-    localStorage.setItem('cookiesDecision', decision);
-    cookieBanner.classList.add('hidden');
-  }
-
   acceptBtn.addEventListener('click', () => {
-    handleDecision('accepted');
-  });
-
-  rejectBtn.addEventListener('click', () => {
-    handleDecision('rejected');
+    localStorage.setItem('cookiesAccepted', 'true');
+    cookieBanner.classList.add('hidden');
   });
 });
